609e53fd0d9e3179187fa9d34a0d4c4bb3d5116a
Дмитрий Гилёв <kem-mit@yandex.ru>
2025-12-21 17:20:00 +0700

    Исправил ошибку линтера

--- src/pages/main/components/offers-content/offers-content.tsx
+++ src/pages/main/components/offers-content/offers-content.tsx
@@ -3,8 +3,6 @@ import PlaceCard from '../../../../components/place-card/place-card';
 import SearchInfo from '../search-info/search-info';
 import SortingForm from '../sorting-form/sorting-form';
 import { Offers } from '../../../../types';
-import { Link } from 'react-router-dom';
-
 
 export default function OffersContent({ offers }: Offers): JSX.Element {
   return (
